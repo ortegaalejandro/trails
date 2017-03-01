a29763d886bab0d16fa81f374e28fafdaa6761bb
Alejandro Ortega <Alejandro@unms-imac>
2017-03-01 14:10:16 -0700

    errors

---
+++ Universal-Guide/SCTTableCell.h
@@ -0,0 +1,20 @@
+//
+//  TableCell.h
+//  Universal-Guide
+//
+//  Created by Alejandro Ortega on 8/10/14.
+//  Copyright (c) 2014 UNMPRC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableCellSCT : UITableViewCell
+
+@property (strong,nonatomic) IBOutlet UILabel *TitleLabelSCT;
+@property (strong,nonatomic) IBOutlet UILabel *DescriptionLabelSCT;
+@property (strong,nonatomic) IBOutlet UIImageView *ThumbImageSCT;
+@property (strong,nonatomic) IBOutlet UIImageView *DogsLabelSCT;
+@property (strong,nonatomic) IBOutlet UIImageView *MapSCT;
+@property (strong,nonatomic) IBOutlet NSString *MapLinkSCT;
+
+@end
---
+++ Universal-Guide/SCTTableViewController.h
@@ -0,0 +1,21 @@
+//
+//  TableViewController.h
+//  Universal-Guide
+//
+//  Created by Alejandro Ortega on 8/10/14.
+//  Copyright (c) 2014 UNMPRC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCTTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *ImagesSCT;
+@property (nonatomic, strong) NSArray *TitleSCT;
+@property (nonatomic, strong) NSArray *DescriptionSCT;
+@property (nonatomic, strong) NSArray *DogsSCT;
+@property (nonatomic, strong) NSArray *MapSCT;
+@property (nonatomic, strong) NSArray *MapLinkSCT;
+
+
+@end
